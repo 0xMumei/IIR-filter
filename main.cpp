420d876ccf74d684c2ab5c7556d8a54c83712102
0xMumei <psicho0202@gmail.com>
2019-12-16 16:49:22 +0300

    Main changes

--- main.cpp
+++ main.cpp
@@ -12,6 +12,20 @@ int inputarray(int *array, int arraysize){
   return 1;
 };
 
+void inputBV(int *array, int arraysize){
+    int TypeN;
+    if(scanf("%i",&TypeN) == 1){
+      if (TypeN == 1){
+      printf("Enter numbers of the array:\n");
+      inputarray(array, arraysize);
+      }else{
+        std::fill(array, array+arraysize, 0);
+      }
+    }else{
+      throw std::invalid_argument("Input is not an integer");
+    }
+}
+
 int inputarraysize(){
   int arraysize;
   if(scanf("%i",&arraysize) == 1){
@@ -34,23 +48,27 @@ int arrayswap(int *array,int arraysize){
   return *arraytemp;
 }
 
-void filter(int *arrayx, int *arraya, int *arrayb, int *arrayA, int sizeofarr, int N) //Use only needed input
+void filter(int *arrayx, int xsize , int *arraya, int asize, int *arrayb, int bsize, int sizeofarr, int N, int *BV_A, int BV_Asize, int *BV_B, int BV_Bsize)
 {   
     //Send needed N step to be printed on inputs of function -/- make it more universal
     //Move middle-step inside the function
     int arrayY[10];
     int arrayF[10];
+    int Output[N+10];
     int Y=0;
     int F=0;
     int r;
     int e;
     //Add bool for choise will we use values x(-1)
-    for (r=0; r<sizeofarr; r++) {
+    for (r=0; r<N+1; r++) {
+        if (N+1>xsize){
+          throw std::runtime_error("Stepsize >> arrayx");
+        }
         for (e=0; e<=r; e++) {
-            Y+=arraya[e]*arrayx[r-e]; //add 0-values as : a_0+arraya[e]*arrayx[r-e]
+            Y+=arraya[e]*arrayx[r-e] + BV_B[e]; //add 0-values as : a_0+arraya[e]*arrayx[r-e]
             //printf("%i*%i + ",arraya[e],arrayx[r-e]);
             if (r-e-1 >= 0) {
-                F+=arrayb[e]*arrayA[r-e-1];//add 0-values just as : b_0+arrayb[e]*arrayA[r-e-1]
+                F+=arrayb[e]*Output[r-e-1] + BV_A[e];//add 0-values just as : b_0+arrayb[e]*arrayA[r-e-1]
                 //printf("%i*%i + ",arrayb[e],arrayA[r-e-1]);
                 }
             if (r-e-1 < 0) {
@@ -60,38 +78,42 @@ void filter(int *arrayx, int *arraya, int *arrayb, int *arrayA, int sizeofarr, i
         }
         arrayY[r]=Y;
         arrayF[r]=F;
-        arrayA[r]=arrayY[r]+arrayF[r];
+        Output[r]=arrayY[r]+arrayF[r];
         //printf(" = %i\n",arrayA[r]);
         Y=0;
         F=0;
     }
-printf("output : [%i] \n",arrayA[N]);
+printf("output : [%i] \n",Output[N]);
 //Print only needed (N) value output of filter
 }
 
 
 int main(int argc, const char * argv[]) {
-    int arrayx[10]={1,4,3,5,6}; //send size of array to func
-    int arraya[10]={1,3,-2,4};
-    int arrayb[10]={4,-2,3};
-    int arrayA[10]={0};
+    int xsize = 10;
+    int arrayx[xsize]={1,4,3,5,6}; //send size of array to func
+    int asize = 10;
+    int arraya[asize]={1,3,-2,4};
+    int bsize = 10;
+    int arrayb[bsize]={4,-2,3};
     int i;
     int N = 2;
 
     int TypeN;
-    int BV_Asize=3;    
     
     printf("While answering the questions answer 1 if yes and 0 if no\n");
 
-    printf("Enter BV_A size!\n");
-    BV_Asize=inputarraysize();
 
+    int BV_Asize=sizeof(arraya)/sizeof(int)-1;
+    int BV_Bsize=sizeof(arrayb)/sizeof(int)-1;
     int BV_A[BV_Asize]; //This array should be size of (a-1) cuz we use all of the a values for 0-values
+    int BV_B[BV_Bsize];
+    int ENDX;
     
-    if (sizeof(BV_A)>0){
-    printf("Enter BV_A values!\n");
-    inputarray(BV_A, BV_Asize);
-    }
+    printf("Enter values of BV_A?\n");
+    inputBV(BV_A,BV_Asize);
+
+    printf("Enter values of BV_B?\n");
+    inputBV(BV_B,BV_Bsize);
 
     if (sizeof(arrayx) < sizeof(arraya) + 1) {
     printf("Array X < Array A + 1, fill left with 0's?\n");
@@ -115,12 +137,6 @@ int main(int argc, const char * argv[]) {
     }
     }//fill rest of the arrayx with 0's
 
-    for (i = 0; i<sizeof(BV_A)/sizeof(int); i++){
-    if (BV_A[i] != 0){
-        throw std::invalid_argument("BV_A values is not null");
-    }
-    }
-
     printf("Enter step you want to see:");
     if(scanf("%i",&N) == 1){
     }
@@ -129,7 +145,7 @@ int main(int argc, const char * argv[]) {
     }
     
 
-    filter(arrayx, arraya, arrayb, arrayA, sizeof(arraya)/sizeof(int), N);
+    filter(arrayx, xsize, arraya, asize, arrayb, bsize, sizeof(arraya)/sizeof(int), N, BV_A, BV_Asize, BV_B, BV_Bsize);
     
 return 0;
 }
